8ec05f1e8939a8c105af5e539007826366396e2f
Galen Ferrara <galen.ferrara557@gmail.com>
2025-08-24 10:03:28 +0100

    Added script to count letters in wordle answers

--- count_letters.py
+++ count_letters.py
@@ -1,3 +1,12 @@
+"""
+--------- COUNT LETTERS SCRIPT ---------
+
+Here we do a small analysis on the answers word list, that is the list of all valid Wordle answers. 
+
+We sort through and count every letter in each position. That is how many times is 'A' the first 
+letter, then 'B', then 'C' and so on. This is outputted to txt file, ./data/letter_counts.txt
+"""
+
 # Answers are all possible wordle answer about 2000 words.
 with open("./answers.txt") as f:
     ANSWERS = [w.strip().upper() for w in f if len(w.strip()) == 5]
@@ -5,16 +14,19 @@ with open("./answers.txt") as f:
     for word in ANSWERS:
         for letter in set(word):
             LETTER_COUNTS[letter] += 1
-    
-    #Sort letter counts by values
-    sorted_letter_counts = dict(sorted(LETTER_COUNTS.items(), key=lambda item: item[1], reverse=True))
-    print(sorted_letter_counts)
+
+    # Sort letter counts by values
+    sorted_letter_counts = dict(
+        sorted(LETTER_COUNTS.items(), key=lambda item: item[1], reverse=True)
+    )
 
     # Output the most common letter in each position [0-4] to a file
-    with open("position_letter_counts.txt", "w") as out_file:
+    with open("./data/letter_counts.txt", "w") as out_file:
         for i in range(5):
             position_counts = {letter: 0 for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
             for word in ANSWERS:
                 position_counts[word[i]] += 1
-            sorted_position_counts = dict(sorted(position_counts.items(), key=lambda item: item[1], reverse=True))
+            sorted_position_counts = dict(
+                sorted(position_counts.items(), key=lambda item: item[1], reverse=True)
+            )
             out_file.write(f"Position {i}: {sorted_position_counts}\n")
